cd0770cf9af8747773a7e514dc4255fc6493008f
Emre Açar <ea517645@gmail.com>
2023-10-19 19:06:30 +0300

    fix: change the soildity compiler version to ^0.8.19 in the contracts in order to fix error HH606 in the hardhat contract deployment

--- contracts/FakeNFTMarketplace.sol
+++ contracts/FakeNFTMarketplace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 contract FakeNFTMarketplace {
     /// @dev Maintain a mapping of Fake TokenID to Owner addresses
